b99eb5a26f7f6b19a356f70719d9136ab54e802c
Kimsangwon <doncham98@gmail.com>
2023-12-14 02:06:35 +0900

    feat : 쏘리

--- TIP-REVIEW/src/Controller/UserController.java
+++ TIP-REVIEW/src/Controller/UserController.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.lang.reflect.Method;
 
@@ -26,12 +27,17 @@ public class UserController extends HttpServlet {
         this.userDAO = new UserDAO(dbConnection);
     }
     //service
+    @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
 
-        // /User/LoginForm.jsp 실행
-        request.getRequestDispatcher("/User/loginForm.jsp").forward(request,response);
+        // 요청의 HTTP 메서드에 따라 doGet 또는 doPost 호출
+        if (request.getMethod().equalsIgnoreCase("GET")) {
+            doGet(request, response);
+        } else if (request.getMethod().equalsIgnoreCase("POST")) {
+            doPost(request, response);
+        }
     }
 
     //doGet
@@ -75,6 +81,9 @@ public class UserController extends HttpServlet {
             userDAO.addUser(user);
             String resultMessage = "User.java added successfully.";
             request.setAttribute("resultMessage", resultMessage);
+            // 세션 생성
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username); //메인에 보여줌
         } catch (Exception e) {
             e.printStackTrace();
             String resultMessage = "Failed to add user.";
@@ -82,7 +91,7 @@ public class UserController extends HttpServlet {
         }
 
         //request.getRequestDispatcher("/result.jsp").forward(request, response);
-        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+        request.getRequestDispatcher("/storeList.jsp").forward(request, response);
     }
 
     private void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -96,18 +105,20 @@ public class UserController extends HttpServlet {
 
                 String resultMessage = "Login successful. User.java ID: " + userId;
                 request.setAttribute("resultMessage", resultMessage);
-                request.setAttribute("username", username); //화면에 보여줄 유저 아이디
 
-                //유저 기본키
-                request.setAttribute("userId", userId); //이 값으로 마이페이지 정보에 접근
+
+                // 세션 생성
+                HttpSession session = request.getSession();
+                session.setAttribute("userId", userId);
+                session.setAttribute("username", username); //메인에 보여줌
 
                 //성공 시 메인페이지로 이동
-                request.getRequestDispatcher("/hello.jsp").forward(request, response);
+                request.getRequestDispatcher("/storeList.jsp").forward(request, response);
             } else {
                 String resultMessage = "Login failed. Invalid credentials.";
                 request.setAttribute("resultMessage", resultMessage);
                 System.out.println("로그인 실패!!");
-                request.getRequestDispatcher("/User.java/loginForm.jsp").forward(request, response);
+                request.getRequestDispatcher("/User/loginForm.jsp").forward(request, response);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -116,6 +127,19 @@ public class UserController extends HttpServlet {
         }
 
         //request.getRequestDispatcher("/result.jsp").forward(request, response);
+    }
+
+    // 세션 삭제 메서드
+    private void logout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+
+        // 세션이 존재하고 username 속성이 있다면 해당 속성을 삭제합니다.
+        if (session != null) {
+            session.removeAttribute("username");
+        }
+
+        // 원하는 페이지로 리다이렉트할 수 있습니다.
+        response.sendRedirect("/storeList.jsp");
     }
 
 }
